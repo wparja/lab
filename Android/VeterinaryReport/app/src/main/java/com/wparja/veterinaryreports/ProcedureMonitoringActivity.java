b7593040a886f1a7d235fa0d7887b9154bd15eeb
Wagner <wagner.arja@hexagon.com>
2021-10-31 11:32:16 -0300

    added vector images

---
+++ Android/VeterinaryReport/app/src/main/java/com/wparja/veterinaryreports/ProcedureMonitoringActivity.java
@@ -0,0 +1,14 @@
+package com.wparja.veterinaryreports;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProcedureMonitoringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_procedure_monitoring);
+    }
+}
