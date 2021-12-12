bedb2d6445caa8c643afa5f19afc5b2cb4013030
Wagner <wagner.arja@hexagon.com>
2021-12-12 19:54:38 -0300

    added new postgres sql queries

---
+++ Postgres/PostgresSQLAdvancedQueriesCreateTables.sql
@@ -0,0 +1,125 @@
+﻿
+-- FILM TABLE
+CREATE TABLE public.film
+(
+  film_id serial,
+  title text,
+  description text,
+  release_year integer,
+  rental_duration smallint,
+  lenght smallint,
+  rental_rate numeric(4,2),
+  CONSTRAINT film_pkey PRIMARY KEY (film_id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+-- ACTOR TABLE
+CREATE TABLE public.actor
+(
+  actor_id integer ,
+  first_name character varying(45),
+  last_name character varying(45),
+  CONSTRAINT actor_pkey PRIMARY KEY (actor_id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- ########################################################################
+-- RENTAL TABLE
+CREATE TABLE public.rental
+(
+  rental_id serial,
+  rental_date timestamp without time zone,
+  inventory_id integer,
+  customer_id smallint,
+  return_date timestamp without time zone,
+  staff_id smallint,
+  last_update timestamp without time zone,
+  CONSTRAINT rental_pkey PRIMARY KEY (rental_id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Find out total rental payment collected from customers
+-- ########################################################################
+-- PAYMENT TABLE
+CREATE TABLE public.payment
+(
+  payment_id integer NOT NULL DEFAULT nextval('payment_payment_id_seq'::regclass),
+  customer_id smallint,
+  staff_id smallint,
+  rental_id integer,
+  amount numeric(5,2),
+  payment_date timestamp without time zone,
+  CONSTRAINT payment_pkey PRIMARY KEY (payment_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.payment
+  OWNER TO postgres;
+ALTER TABLE public.rental
+  OWNER TO postgres;
+INSERT INTO rental (customer_id,inventory_id,  return_date, staff_id, last_update)
+    VALUES (28, '15/09/2021 23:49:21',2,'15/09/2021 23:49:21');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('14/08/2021 23:19:53',25,24, '19/08/2021 23:19:53',2,'19/08/2021 23:19:53');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('18/03/2021 14:58:36',33,25, '22/03/2021 14:58:36',2,'22/03/2021 14:58:36');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('22/01/2021 09:28:35',26,31, '25/01/2021 09:28:35',4,'25/01/2021 09:28:35');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('27/06/2021 06:13:02',42,16, '02/07/2021 06:13:02',1,'02/07/2021 06:13:02');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('04/07/2021 14:40:31',50,45, '10/07/2021 14:40:31',4,'10/07/2021 14:40:31');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('07/04/2021 06:38:40',14,47, '12/04/2021 06:38:40',4,'12/04/2021 06:38:40');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('20/03/2021 05:45:57',28,48, '25/03/2021 05:45:57',1,'25/03/2021 05:45:57');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('24/07/2021 13:04:09',36,8, '30/07/2021 13:04:09' ,3,'30/07/2021 13:04:09');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('25/04/2021 06:26:05',48,1, '30/04/2021 06:26:05' ,2,'30/04/2021 06:26:05');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('23/11/2021 21:52:54',20,13, '28/11/2021 21:52:54',2,'28/11/2021 21:52:54');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('11/03/2021 11:13:25',34,21, '18/03/2021 11:13:25',1,'18/03/2021 11:13:25');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('08/11/2021 04:02:23',8, 2, '12/11/2021 04:02:23' ,4,'12/11/2021 04:02:23');  
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('05/06/2021 22:28:51',4, 46, '10/06/2021 22:28:51',1,'10/06/2021 22:28:51');  
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('17/05/2021 03:54:56',16,7, '23/05/2021 03:54:56' ,4,'23/05/2021 03:54:56');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('07/02/2021 15:47:46',6, 41, '12/02/2021 15:47:46',1,'12/02/2021 15:47:46');  
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('19/07/2021 06:29:10',31,39, '23/07/2021 06:29:10',2,'23/07/2021 06:29:10');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('10/05/2021 12:41:03',12,26, '15/05/2021 12:41:03',3,'15/05/2021 12:41:03');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('10/06/2021 06:56:54',40,37, '15/06/2021 06:56:54',4,'15/06/2021 06:56:54');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('09/10/2021 08:36:43',10,12, '13/10/2021 08:36:43',3,'13/10/2021 08:36:43');   
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('09/10/2021 08:36:43',10,12, '13/10/2021 08:36:43',3,'13/10/2021 09:36:43');  
+INSERT INTO rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+    VALUES ('09/10/2021 08:36:43',10,12, '13/10/2021 09:36:43',3,'13/10/2021 09:36:43');  
+
+
+
+-- Find area of the store with diameter 500 feet
+/*
+A = PI * (radius)^2;
+A = PI * (diameter/2)^2;
+*/
+
+SELECT PI()*(500/2)^2 AS AreaofCircle;
+
+
+
+
--- Postgres/PostgresSQLAdvancedQueriesFetchTables.sql
+++ Postgres/PostgresSQLAdvancedQueriesFetchTables.sql
@@ -1,58 +1,135 @@
-﻿
-do $$
+﻿CREATE OR REPLACE FUNCTION getFirstName() RETURNS TEXT AS $$
 DECLARE
 first_names TEXT[];
-last_names TEXT[];
-begin
-first_names := ARRAY[
+first_name TEXT;
+_index INTEGER;
+    BEGIN
+     first_names := ARRAY[
 	    'Adam','Bill','Bob','Calvin','Donald','Dwight','Frank','Fred','George','Howard',
 	    'James','John','Jacob','Jack','Martin','Matthew','Max','Michael',
 	    'Paul','Peter','Phil','Roland','Ronald','Samuel','Steve','Theo','Warren','William',
 	    'Abigail','Alice','Allison','Amanda','Anne','Barbara','Betty','Carol','Cleo','Donna',
 	    'Jane','Jennifer','Julie','Martha','Mary','Melissa','Patty','Sarah','Simone','Susan'
 	    ];
+	    _index := random() * ARRAY_LENGTH(first_names,1);
+	    IF _index = 0 THEN
+	      _index := 1;
+	    END IF;
+	    first_name := first_names[_index];
+	    raise notice 'index first name % - size % name %', _index, ARRAY_LENGTH(first_names,1), first_name;
+        RETURN first_name;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION getLastName() RETURNS TEXT AS $$
+DECLARE
+last_names TEXT[];
+last_name TEXT;
+_index INTEGER;
+    BEGIN
     last_names := ARRAY[
 		'Matthews','Smith','Jones','Davis','Jacobson','Williams','Donaldson','Maxwell','Peterson','Stevens',
 		'Franklin','Washington','Jefferson','Adams','Jackson','Johnson','Lincoln','Grant','Fillmore','Harding','Taft',
 		'Truman','Nixon','Ford','Carter','Reagan','Bush','Clinton','Hancock'
 	    ];
-	    
-    for i IN 1..10 loop
+	    _index := random() * ARRAY_LENGTH(last_names,1);
+	    IF _index = 0 THEN
+	      _index := 1;
+	    END IF;
+	    last_name := last_names[_index];
+	    raise notice 'index last name % - size % name %', _index, ARRAY_LENGTH(last_names,1), last_name;
+
+        RETURN last_name;
+    END;
+$$ LANGUAGE plpgsql;
+	
+-- fetch the actor table
+do $$
+Declare 
+ firstName TEXT;
+ lastName TEXT;
+begin	    
+    for i IN 1..100 loop
+          firstName := getFirstName();
+          lastName := getLastName();                    
 	 INSERT INTO actor(actor_id, first_name, last_name) values 
-	 (i, 
-	 first_names[random() * ARRAY_LENGTH(first_names,1)], 
-	first_names[random() * ARRAY_LENGTH(first_names,1)]);
+	 (	i, 
+		firstName, 
+		lastName
+	);
     end loop;
 end;
 $$
 
--- select actor_id from actor order by random() limit 1
 
+CREATE OR REPLACE FUNCTION getFilmTitle() RETURNS TEXT AS $$
+DECLARE
+film_titles TEXT[];
+_index INTEGER;
+BEGIN
+	film_titles := ARRAY['The Shawshank Redemption', 'The Godfather', 'The Godfather: Part II', 'The Dark Knight', '12 Angry Men', 'Schindler''s List','Pulp Fiction', 'The Good, the Bad and the Ugly', 
+	'The Lord of the Rings: The Return of the King', 'Fight Club', 'The Lord of the Rings: The Fellowship of the Ring', 'Star Wars: Episode V - The Empire Strikes Back', 'Forrest Gump', 'Inception', 
+	'One Flew Over the Cuckoo''s Nest', 'The Lord of the Rings: The Two Towers', 'Goodfellas', 'The Matrix', 'Star Wars', 'Seven Samurai', 'City of God', 'Se7en', 'The Silence of the Lambs', 'The Usual Suspects', 
+	'It''s a Wonderful Life', 'Life Is Beautiful', 'Léon: The Professional', 'Once Upon a Time in the West', 'Interstellar', 'Saving Private Ryan', 'American History X', 'Spirited Away', 'Casablanca', 
+	'Raiders of the Lost Ark', 'Psycho', 'City Lights', 'Rear Window', 'The Intouchables', 'Modern Times', 'Terminator 2: Judgment Day', 'Whiplash', 'The Green Mile', 'The Pianist', 'Memento', 'The Departed', 
+	'Gladiator', 'Apocalypse Now', 'Back to the Future', 'Sunset Blvd.', 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', 'The Prestige', 'Alien', 'The Lion King', 'The Lives of Others', 
+	'The Great Dictator', 'Inside Out', 'Cinema Paradiso', 'The Shining', 'Paths of Glory', 'Django Unchained', 'The Dark Knight Rises', 'WALL·E', 'American Beauty', 'Grave of the Fireflies', 'Aliens', 'Citizen Kane', 
+	'North by Northwest', 'Princess Mononoke', 'Vertigo', 'Oldeuboi', 'Das Boot', 'M', 'Star Wars: Episode VI - Return of the Jedi', 'Once Upon a Time in America', 'Amélie', 'Witness for the Prosecution', 'Reservoir Dogs', 
+	'Braveheart', 'Toy Story 3', 'A Clockwork Orange', 'Double Indemnity', 'Taxi Driver', 'Requiem for a Dream', 'To Kill a Mockingbird', 'Lawrence of Arabia', 'Eternal Sunshine of the Spotless Mind', 'Full Metal Jacket', 
+	'The Sting', 'Amadeus', 'Bicycle Thieves', 'Singin'' in the Rain', 'Monty Python and the Holy Grail', 'Snatch.', '2001: A Space Odyssey', 'The Kid', 'L.A. Confidential', 'Rashômon', 'For a Few Dollars More', 'Toy Story', 
+	'The Apartment', 'Inglourious Basterds', 'All About Eve', 'The Treasure of the Sierra Madre', 'Jodaeiye Nader az Simin', 'Indiana Jones and the Last Crusade', 'Metropolis', 'Yojimbo', 'The Third Man', 'Batman Begins', 
+	'Scarface', 'Some Like It Hot', 'Unforgiven', '3 Idiots', 'Up', 'Raging Bull', 'Downfall', 'Mad Max: Fury Road', 'Jagten', 'Chinatown', 'The Great Escape', 'Die Hard', 'Good Will Hunting', 'Heat', 'On the Waterfront'];
+	_index := random() * ARRAY_LENGTH(film_titles, 1);
+	IF _index = 0 THEN 
+	   _index := 1;
+	END IF;
+	RETURN film_titles[_index];
+END;
+$$ LANGUAGE plpgsql;
+
+-- fetch the film table
+do $$
+Declare
+	filmTitle TEXT;
+BEGIN
+	FOR i IN 1..10 LOOP
+	filmTitle := getFilmTitle();                    
+	 INSERT INTO 
+		film( title, description, release_year, rental_duration, lenght, rental_rate) 
+	 VALUES 
+		(filmTitle, filmTitle,(SELECT floor(random() * 20 + 1)::int + 2000), (SELECT random() * 7), ((SELECT random() * 100) + 100), (SELECT round((random()* 9)::NUMERIC, 2)));
+	END LOOP;
+END;
+$$
+
+-- fetch rental table
+
+do $$
+Declare
+	_rental_date TIMESTAMP;
+BEGIN
+	FOR i IN 1..10 LOOP
+	_rental_date := (select timestamp '2014-01-10 08:00:00' + random() * (timestamp '2021-01-20 18:00:00' - timestamp '2014-01-10 08:00:00'));                    
+	 INSERT INTO 
+		rental (rental_date, inventory_id, customer_id, return_date, staff_id, last_update)
+	VALUES 
+		(_rental_date, (SELECT random() * 100), (SELECT random() * 500), (SELECT _rental_date + interval '4 day'), (SELECT random() * 4), (SELECT _rental_date + interval '4 day'));
+	END LOOP;
+END;
+$$
+
+SELECT (random() * 20.0)
+
+SELECT floor(random() * 20)::int + 2000
+
+SELECT round((random()* 9)::NUMERIC, 2)
+
+SELECT to_char((random() * 9.0),'FM999999990.00')
+
+select timestamp '2014-01-10 08:00:00' +
+       random() * (timestamp '2021-01-20 18:00:00' -
+                   timestamp '2014-01-10 08:00:00')
+select actor_id from actor order by random() limit 1
+SELECT generate_series(date'2010-01-20',date'2010-02-04','1 day');
    
 
-FOR i IN 1..10 LOOP
- -- Fetch actor table
- INSERT INTO actor(actor_id, first_name, last_name) values (i, 
-  arrays.firstnames[s.a % ARRAY_LENGTH(arrays.firstnames,1) + 1], 
- (SELECT array_to_string(ARRAY(SELECT chr((65 + round(random() * 25)) :: integer) FROM generate_series(1,15)), '')));
-END LOOP;
-
-SELECT
-    arrays.firstnames[s.a % ARRAY_LENGTH(arrays.firstnames,1) + 1] AS firstname,
-    substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ' from s.a%26+1 for 1) AS middlename,
-    arrays.lastnames[s.a % ARRAY_LENGTH(arrays.lastnames,1) + 1] AS lastname
-FROM     generate_series(1,300) AS s(a) -- number of names to generate
-CROSS JOIN(
-    SELECT ARRAY[
-    'Adam','Bill','Bob','Calvin','Donald','Dwight','Frank','Fred','George','Howard',
-    'James','John','Jacob','Jack','Martin','Matthew','Max','Michael',
-    'Paul','Peter','Phil','Roland','Ronald','Samuel','Steve','Theo','Warren','William',
-    'Abigail','Alice','Allison','Amanda','Anne','Barbara','Betty','Carol','Cleo','Donna',
-    'Jane','Jennifer','Julie','Martha','Mary','Melissa','Patty','Sarah','Simone','Susan'
-    ] AS firstnames,
-    ARRAY[
-        'Matthews','Smith','Jones','Davis','Jacobson','Williams','Donaldson','Maxwell','Peterson','Stevens',
-        'Franklin','Washington','Jefferson','Adams','Jackson','Johnson','Lincoln','Grant','Fillmore','Harding','Taft',
-        'Truman','Nixon','Ford','Carter','Reagan','Bush','Clinton','Hancock'
-    ] AS lastnames
-) AS arrays
